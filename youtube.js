ce8c37ec18df70361fb85aca121c8a3061aa152f
Josh Oliver <31630195+textmonster404@users.noreply.github.com>
2019-07-21 14:34:39 -0700

    Added semicolon

--- youtube.js
+++ youtube.js
@@ -1,5 +1,5 @@
 for (var i=0;i<document.getElementsByTagName("youtube").length;i++) {
     var tag = document.getElementsByTagName("youtube")[i];
-    tag.innerHTML = `<iframe width="420" height="315" src="`+"https://www.youtube.com/embed/"+(new URL(tag.getAttribute("href")).searchParams.get("v"))+`"></iframe></td>`
+    tag.innerHTML = `<iframe width="420" height="315" src="`+"https://www.youtube.com/embed/"+(new URL(tag.getAttribute("href")).searchParams.get("v"))+`"></iframe></td>`;
     tag.style.display = "content";
 }
